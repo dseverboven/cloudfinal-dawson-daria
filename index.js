bd67917cd70e9e25e887a1c6d975e54eb1e553d2
Dawson Verboven <dseverboven@gmail.com>
2020-04-06 12:39:12 -0700

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+let express = require('express');  
+let app = express();  
+let bodyParser = require('body-parser');
+let path = require('path');
+var Request = require("request");
+
+
+app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, 'routes')));
+
+
+
+Request.post({
+    "headers": { "content-type": "application/json" },
+    "url": "https://ovc5idyes0.execute-api.us-east-1.amazonaws.com/dev-1/",
+    "body": JSON.stringify({"instances": [{"artist":"Migos", "seed": "corn"}]})
+}, (error, response, body) => {
+    if(error) {
+        return console.dir(error);
+    }
+    console.dir(JSON.parse(body));
+});
+
+
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname,'index.html')));  
+app.listen(process.env.PORT || 3000); 
---
+++ routes/api.js
@@ -0,0 +1,51 @@
+
+
+
+
+async function generate() {
+
+    console.log('hi')
+    var new_actor = await fetch("/new")
+    
+
+}
+
+// const userAction = async () => {
+//     myBody = {"instances": [{"artist":"Migos", "seed": "corn"}]}
+//     const response = await fetch('https://ovc5idyes0.execute-api.us-east-1.amazonaws.com/dev-1/', {
+//         headers: {
+//             'Content-Type': 'application/json', 
+//             'Access-Control-Allow-Origin': '*' ,
+            
+//         },
+//       method: 'POST',
+//       credentials: "omit",
+//       mode:'cors',
+//       body: JSON.stringify(myBody), // string or object
+      
+//     });
+//     const myJson = await response.json(); //extract JSON from the http response
+//     console.log(myJson)
+//   }
+   
+
+//     // var input, filter, a;
+//     // var input = document.getElementById('search');
+//     // var filter = input.value.toLowerCase();
+
+   
+//     //     newnew = await fetch("/search", {
+//     //         method: "POST",
+//     //         body: JSON.stringify([filter]),
+//     //         headers: {
+//     //             'Content-Type': 'application/json'
+//     //         },
+//     //     })
+
+//     //     local_actor = await newnew.json();
+
+//     //     display();
+    
+
+
+    
